1f2881b1bad7f46b4ec88ac4df5c86361b6f2608
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-06-14 05:43:45 -0400

    0.1 - Succubi

--- dwmx/.dwm/autostart.sh
+++ dwmx/.dwm/autostart.sh
@@ -3,12 +3,8 @@
 exec warpd &
 # Execute nitrogen to restore the set wallpaper.
 exec nitrogen --restore &
-# Ghost girl?
-exec oneko -tomoyo &
 
 # Add clock to bar
-
-
 
 while true; do
 	
@@ -17,7 +13,7 @@ while true; do
 	micvolume=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $4":"$6 }')	
 	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
 	battery=$(cat /sys/class/power_supply/BAT0/capacity)
-	clock=$(date +'%F %H:%M:%S.%N')
+	clock=$(date +'%m-%d %H:%M:%S.%N')
 
 	xsetroot -name " T: $temperature |  M: $micvolume | $clock | V: $volume | B: $battery% |" &
 	
