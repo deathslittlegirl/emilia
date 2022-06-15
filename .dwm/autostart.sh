2747ba71bb8440f4da4c587873bd0b1b03c20013
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-06-15 13:24:02 -0400

    0.2 - Cosplay

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# set xsetroot bar. 
+#* set xsetroot bar. *#
+
 while true; do
 	
 	memory=$(free -h | awk '(NR==2){ print $4 }')
-	volume=$(amixer get -c 2 Master | awk -F'[][]' 'END{ print $4":"$6 }')
-	micvolume=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $4":"$6 }')	
+	volume=$(amixer get -c 2 Master | awk -F'[][]' 'END{ print $6":"$2 }')
+	micvolume=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6":"$2 }')	
 	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
 	battery=$(cat /sys/class/power_supply/BAT0/capacity)
-	clock=$(date +'%m-%d %H:%M:%S.%N')
-
-	xsetroot -name " T: $temperature | M: $micvolume | $clock | V: $volume | B: $battery% |" &
+	clock=$(date +'%a %m-%d %H:%M:%S.%N')	
+	xsetroot -name " T: $temperature | M: $micvolume | $clock | V: $volume | $battery% " &
 	
 	wait
 
@@ -23,5 +23,4 @@ exec nitrogen --restore &
 
 # execute warpd for keyboard-based mouse control.
 
-# exec warpd &
-
+exec warpd &
