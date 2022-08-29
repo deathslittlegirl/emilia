edc32a0580576efa2b46b6c3d54a4ca69396864a
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-08-28 20:25:22 -0400

    0.5 - Pure Free

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-#* set xsetroot bar. *#
+exec dunst &
+exec nitrogen --restore &
+exec xcompmgr -f -D 5 &
+
+exec amixer set -c 2 'Internal Mic Boost' 0 &
+exec amixer set -c 2 'Capture' 52 & 
+exec amixer cset -c 2 numid=8,iface=MIXER,name='Capture Switch' off &
 
 while true; do
 	
-	memory=$(free -h | awk '(NR==2){ print $4 }')
-	volume=$(amixer get -c 2 Master | awk -F'[][]' 'END{ print $6":"$2 }')
+	memory=$(free -h | awk '(NR==2){ print $3 }')
+
+	volume=$(amixer get Master | awk -F'[][]' 'END{print $2 }')
+
 	micvolume=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6":"$2 }')	
+	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
+	
 	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
+
 	battery=$(cat /sys/class/power_supply/BAT0/capacity)
-	clock=$(date +'%a %m-%d %H:%M:%S.%N')	
-	xsetroot -name " T: $temperature | M: $micvolume | $clock | V: $volume | $battery% " &
+
+	clock=$(date +'%m-%d %a %H:%M:%S:%N')	
+
+	xsetroot -name " T: $temperature | M: $micvolume | V: $volume | B: $battery% :: $clock " &
 	
 	wait
-
 done &
 
-# execute nitrogen to restore the set wallpaper.
-
-exec nitrogen --restore &
-
-
-# execute warpd for keyboard-based mouse control.
-
-exec warpd &
