d6f53683fbe8df144f87b8da9109d5998b7109d2
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-10-04 18:08:46 -0400

    0.7 - butterfly

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-exec killall warpd & 
+killall python & 
+killall warpd & 
+exec pkill warpd &
 exec dunst &
 exec /usr/libexec/kdeconnectd & 
 exec nitrogen --restore &
@@ -14,29 +16,48 @@ exec amixer cset -c 2 numid=10,iface=MIXER,name='Internal Mic Boost Volume' 0 &
 
 exec amixer cset -c 2 numid=7,iface=MIXER,name='Capture Volume' 53 & 
 
-# exec amixer set -c 3 'Mic' 77 & 
-
 exec amixer cset -c 2 numid=8,iface=MIXER,name='Capture Switch' off &
-
-# exec amixer cset -c 3 numid=7,iface=MIXER,name='Mic Capture Switch' off &
 
 while true; do
 	
 	memory=$(free -h | awk '(NR==2){ print $3 }')
 
-	volume=$(amixer get -c 2 Master | awk -F'[][]' 'END{print $2 }')
+	speaker=$(amixer get -c 2 Master | awk -F'[][]' 'END{print $2 }')
 
-	micvolume=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
-	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
-	
+	microphone=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
+
 	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
 
 	battery=$(cat /sys/class/power_supply/BAT0/capacity)
 
-	clock=$(date +'%m::%d %a %H:%M:%S::%N')	
+	clock=$(date +'[%m%d.%H%M.%S%N]')	
 
-	xsetroot -name " T::$temperature | M: $micvolume | V::$volume | B::$battery% | $clock " &
+	xsetroot -name " T::$temperature | M: $microphone | S::$speaker | B::$battery% | $clock " &
 	
 	wait
 done &
 
+while true; do
+
+    cd $HOME/.var/app/com.discordapp.Discord
+        rm -rf cache/
+        cd $HOME
+	sleep 120 &
+	wait
+done &	
+
+while true; do
+
+        
+	liapeak=$(amixer get -c 2 'Internal Mic Boost' | awk -F'[][]' 'END{print $2 }')
+
+	if [ $liapeak != "0%" ]; then
+		exec amixer cset -c 2 numid=10,iface=MIXER,name='Internal Mic Boost Volume' 0 & 
+		exec amixer cset -c 2 numid=7,iface=MIXER,name='Capture Volume' 53 & 
+	fi
+
+	wait
+done & 
+
+
+ 
