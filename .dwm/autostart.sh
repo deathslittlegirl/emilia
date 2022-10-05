0ef38bc98db0975142309f63ce75686c39db38bb
deathslittlegirl <90430427+deathslittlegirl@users.noreply.github.com>
2022-10-05 04:45:03 -0400

    0.7

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -25,7 +25,8 @@ while true; do
 	speaker=$(amixer get -c 2 Master | awk -F'[][]' 'END{print $2 }')
 
 	microphone=$(amixer get -c 2 Capture | awk -F'[][]' 'END{ print $6"::"$2 }')	
-
+	#micbvolume=$(amixer get -c 3 Mic | awk -F '[][]' 'END{ print $6":"$2 }')
+	
 	temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)
 
 	battery=$(cat /sys/class/power_supply/BAT0/capacity)
@@ -50,14 +51,17 @@ while true; do
 
         
 	liapeak=$(amixer get -c 2 'Internal Mic Boost' | awk -F'[][]' 'END{print $2 }')
-
+	lialoud=$(amixer get -c 2 'Capture' | awk -F'[][]' 'END{print $2 }')
+	
 	if [ $liapeak != "0%" ]; then
 		exec amixer cset -c 2 numid=10,iface=MIXER,name='Internal Mic Boost Volume' 0 & 
 		exec amixer cset -c 2 numid=7,iface=MIXER,name='Capture Volume' 53 & 
+	
+	elif [ $lialoud == "100%" ]; then
+
+	    exec amixer cset -c 2 numid=7,iface=MIXER,name='Capture Volume' 53 &
+
 	fi
-
-	wait
+	
+       wait
 done & 
-
-
- 
